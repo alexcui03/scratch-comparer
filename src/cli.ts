c3d4d3f66e811943bf18cbd6fcb72550de18374b
Alex Cui <cuizhihui030925@outlook.com>
2020-08-15 16:52:05 +0800

    Added CLI

--- src/cli.ts
+++ src/cli.ts
@@ -4,14 +4,28 @@ import LcsCodeComparer from './comparer/code_lcs';
 import yargs = require('yargs');
 import util = require('util');
 
-const argv = yargs.options({
-    src: { type: 'string' },
-    dst: { type: 'string' }
-}).argv;
+const argv = yargs
+    .usage('Usage: $0 [options]')
+    .alias('v', 'version')
+    .alias('s', 'src')
+    .describe('s', 'Source file path')
+    .alias('d', 'dst')
+    .describe('d', 'Destination file path')
+    .alias('p', 'precision')
+    .describe('p', 'Decimal precision')
+    .alias('h', 'help')
+    .help('h')
+    .demandOption(['s', 'd'])
+    .epilog('Write by Alex Cui')
+    .options({
+        src: { type: 'string' },
+        dst: { type: 'string' },
+        precision: { type: 'number', default: 4 }
+    }).argv;
 
 let srcLoader = new SB3Loader(argv.src);
 let dstLoader = new SB3Loader(argv.dst);
 
 let comparer = new LcsCodeComparer(srcLoader, dstLoader);
 
-console.log(`Similarity: ${(comparer.compare().similarity * 100).toFixed(4)}%`);
+console.log(`Similarity: ${(comparer.compare().similarity * 100).toFixed(argv.precision)}%`);
